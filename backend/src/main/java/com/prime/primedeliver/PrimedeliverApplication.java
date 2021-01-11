359c23441542d7b5be47324271533d69dc603125
unknown <edsonfire@gmail.com>
2021-01-11 00:25:49 -0300

    projeto criado

---
+++ backend/src/main/java/com/prime/primedeliver/PrimedeliverApplication.java
@@ -0,0 +1,13 @@
+package com.prime.primedeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimedeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimedeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/prime/primedeliver/PrimedeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prime.primedeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimedeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
